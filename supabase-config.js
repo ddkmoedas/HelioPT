50fa6bc8b915c76e859ed3eb35c20761cb1735e8
ddkmoedas <dl9487838@gmail.com>
2026-06-14 22:28:00 +0100

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,4 +0,0 @@
-const SUPABASE_URL = "https://ldqknjzefmnmkqqkkpkm.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_FKKqTIdvkp5VDXxGj6EDWA_s6PUnxOj";
-
-const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
